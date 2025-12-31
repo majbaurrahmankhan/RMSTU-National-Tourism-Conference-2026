fc5fb0e62a4c3815480311c59f54ff7e6bc7e775
majbaurrahmankhan <majbaurkhan@gmail.com>
2026-01-01 00:50:08 +0600

    Update KeyDates.jsx

--- src/components/KeyDates.jsx
+++ src/components/KeyDates.jsx
@@ -9,7 +9,7 @@ import icon4 from '../assets/images/icon4.png';
 const keyDatesData = [
   {
     title: "Abstract Submission Deadline",
-    date: "31 Dec 2025",
+    date: "02 Jan 2026",
     icon: icon1
   },
   {
